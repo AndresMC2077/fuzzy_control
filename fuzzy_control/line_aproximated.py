0d227630488702c97b742e4dc3a842a1f9634f85
Andrés Méndez Cortez <126106627+AndresMC2077@users.noreply.github.com>
2026-06-12 20:00:57 -0600

    Fix lin_aprox

--- fuzzy_control/line_aproximated.py
+++ fuzzy_control/line_aproximated.py
@@ -3,7 +3,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twisthttps://github.com/AndresMC2077/fuzzy_control/pulse
 import math
 import numpy as np
 
@@ -25,10 +25,7 @@ class NonLinearControllerNode(Node):
     def error_callback(self, msg):
         # 1. Recibir error (Asegurando rango seguro -1.0 a 1.0)
         x = np.clip(msg.data, -1.0, 1.0)
-
-        # -------------------------------------------------------------
-        # 2. CÁLCULO DE VELOCIDAD LINEAL (V) -> Perfil Gaussiano
-        # -------------------------------------------------------------
+        # Calculo de vel lineal x
         v_out = 0.0
         
         # Aplicamos el truncado estricto a cero si el desvío es mayor o igual a 0.5
@@ -42,29 +39,19 @@ class NonLinearControllerNode(Node):
             # Ecuación Gaussiana para valores escalares
             v_out = A_v * math.exp(-((x - mu) ** 2) / (2 * sigma ** 2))
 
-        # -------------------------------------------------------------
-        # 3. CÁLCULO DE VELOCIDAD ANGULAR (W) -> Tangente Hiperbólica
-        # -------------------------------------------------------------
+        #Calculo de vel angular w
         A_w = 0.1455  # Amplitud de la tanh
         k = 4.0       # Pendiente / Inclinación
-        
-        # Ojo: Invierte el signo aquí multiplicando por -1 si ves que 
-        # tu robot gira en el sentido opuesto al deseado.
         w_raw = A_w * math.tanh(k * x)
         
         # Aplicar la saturación exacta en los límites cinemáticos
         w_out = np.clip(w_raw, -0.1453, 0.1453)
 
-        # -------------------------------------------------------------
-        # 4. PUBLICAR COMANDO
-        # -------------------------------------------------------------
+        # publish
         twist = Twist()
         twist.linear.x = float(v_out)
         twist.angular.z = float(w_out)
         self.cmd_pub.publish(twist)
-
-        # Descomenta la siguiente línea para hacer debug por consola
-        # self.get_logger().info(f"Error: {x:.3f} | V: {v_out:.3f} | W: {w_out:.3f}")
 
 def main(args=None):
     rclpy.init(args=args)
